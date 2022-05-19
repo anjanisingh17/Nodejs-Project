583b45da9443e61adc0d174de1497b9380a765cb
Anjani <anjanisingh953@gmail.com>
2022-05-19 18:48:02 +0530

    Changes saved

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ app.post('/update_details/:id',upload,(req,res)=>{
             console.log(err);
         }else{
             res.redirect('/show_details');
-            console.log("Hello Git Testing");
+            console.log("Hello Git Changes");
         }
     })
 
