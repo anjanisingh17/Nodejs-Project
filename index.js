fdc18acfa8d57263f2fda2c583f63bea6753b5c8
Anjani <anjanisingh953@gmail.com>
2022-05-20 10:23:22 +0530

    Today Changes

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ app.post('/update_details/:id',upload,(req,res)=>{
             console.log(err);
         }else{
             res.redirect('/show_details');
-            console.log("Hello Git Changes");
+            console.log("Hello Git Changes Today");
         }
     })
 
